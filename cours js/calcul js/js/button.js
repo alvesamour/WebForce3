7fa9a010d9af997f05ab25e80788d96474d2cfa3
Unknown <unknown@example.com>
2017-08-09 17:36:08 +0200

    cours 9/08/2017

---
+++ cours js/calcul js/js/button.js
@@ -0,0 +1,16 @@
+function toggleBackgroundColor (color) {
+    document.body.style.backgroundColor = color;
+    document.body.style.color = "#fff";
+    document.getElementById("btn-blue").style.color = "#fff";
+    var newParagraph = document.createElement("p");
+    newParagraph.innerHTML = "<strong>Du texte</strong>";
+    document.body.appendChild(newParagraph);
+}
+
+window.onload = function () {
+    var blueButton = document.getElementById("btn-blue");
+
+    blueButton.addEventListener("click", function (){
+        toggleBackgroundColor ("blue");
+    });
+}
---
+++ cours js/deplacement/js/deplacement.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    var left = document.getElementById("left");
+    var top = document.getElementById("top");
+    var myDiv = document.getElementById("carré");
+}
+myform.addEventListener ('submit, function (e)') {
+    e.preventDefault();
+    var left = left.value;
+    var top = top.value;
+
+    myDiv.style.left = valleft + "px";
+    myDiv.style.top = valtop + "px";
+)};
+}
