129a4f53af8bc07610ea1a2f7b8e4366781ab2c6
Unknown <unknown@example.com>
2017-10-11 16:57:19 +0200

    cours 11/10/2017

---
+++ AJAX/ajax3/formulaire.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <script type="text/javascript" src=" ajaxJs2.js"></script>
+    </head>
+    <body>
+        <form method="post" action="">
+            <fieldset>
+                <legend>Ajouter un employé</legend>
+
+                <select name="personne" id="personne">
+                    <?php
+                    require_once ('init.inc.php');
+                    $resultat = $pdo -> query("SELECT * FROM employes");
+                        while($employes = $resultat->fetch(PDO::FETCH_ASSOC)){
+                            echo '<option>'  . $employes['prenom'] . '</option>';
+                        }
+                    ?>
+                </select>
+
+                <input type="submit" id="submit" value="ok">
+
+            </fieldset>
+        </form>
+
+        <div id="resultat"></div>
+    </body>
+</html>
